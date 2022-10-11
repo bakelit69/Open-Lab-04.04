59a7fff58077ae84ca81cacf784eb8ebaee98279
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-10-11 09:53:08 +0200

    Update PhoneNumbers.cs

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._04
 {
     public class PhoneNumbers
     {
         public string FormatPhoneNumber(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {         
+            string phonenumber = String.Format("({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6], numbers[7], numbers[8], numbers[9]);
+            return phonenumber;
         }
     }
 }
